9a2505ceadacc9198fedaa30e1234dc1e0073316
olehmartynenko <martynenkooleg77@gmail.com>
2023-05-02 19:49:14 +0300

    Add tests and modified go.sum

---
+++ test/parser_test.go
@@ -0,0 +1,100 @@
+package test
+
+import (
+	"image/color"
+	"strings"
+	"testing"
+
+	"github.com/sueta2016/labik-3/painter"
+	"github.com/sueta2016/labik-3/painter/lang"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_parse_struct(t *testing.T) {
+	tests := []struct {
+		name    string
+		command string
+		op      painter.Operation
+	}{ 
+		{
+			name:    "background rectangle",
+			command: "bgrect 0 0 100 100",
+			op:      &painter.BgRectangle{X1: 0, Y1: 0, X2: 100, Y2: 100},
+		},
+		{
+			name:    "figure",
+			command: "figure 200 200",
+			op:      &painter.Figure{X: 200, Y: 200, C: color.RGBA{R: 255, G: 255, B: 0, A: 1}},
+		},
+		{
+			name:    "move",
+			command: "move 100 100",
+			op:      &painter.Move{X: 100, Y: 100},
+		},
+		{
+			name:    "update",
+			command: "update",
+			op:      painter.UpdateOp,
+		},
+		{
+			name:    "invalid command",
+			command: "invalidcommand",
+			op:      nil,
+		},
+	}
+
+
+	for _, tc := range tests { 
+		t.Run(tc.name, func(t *testing.T) {
+			parser := &lang.Parser{}
+			ops, err := parser.Parse(strings.NewReader(tc.command))
+			if tc.op == nil {                                       
+				assert.Error(t, err) 
+			} else { 
+				require.NoError(t, err)        
+				assert.IsType(t, tc.op, ops[1]) 
+				assert.Equal(t, tc.op, ops[1])  
+			}
+		})
+	}
+}
+
+func Test_parse_func(t *testing.T) {
+	tests := []struct { 
+		name    string
+		command string
+		op      painter.Operation
+	}{
+		{
+			name:    "white fill",
+			command: "white",
+			op:      painter.OperationFunc(painter.WhiteFill),
+		},
+		{
+			name:    "green fill",
+			command: "green",
+			op:      painter.OperationFunc(painter.GreenFill),
+		},
+		{
+			name:    "reset screen",
+			command: "reset",
+			op:      painter.OperationFunc(painter.ResetScreen),
+		},
+	}
+
+	parser := &lang.Parser{}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			ops, err := parser.Parse(strings.NewReader(tc.command))
+
+			
+
+			require.NoError(t, err)        
+			require.Len(t, ops, 1)          
+			assert.IsType(t, tc.op, ops[0]) 
+
+		})
+	}
+}
---
+++ test/test_eventloop.go
@@ -0,0 +1,151 @@
+package test
+
+import (
+	"image"
+	"image/color"
+	"image/draw"
+	"testing"
+	"time"
+
+	"github.com/sueta2016/labik-3/painter"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"golang.org/x/exp/shiny/screen"
+)
+
+type Mock struct {
+	mock.Mock 
+} 
+
+func (_ *Mock) NewBuffer(size image.Point) (screen.Buffer, error) {
+	return nil, nil
+}
+
+func (_ *Mock) NewWindow(opts *screen.NewWindowOptions) (screen.Window, error) {
+	return nil, nil
+}
+
+
+func (mockReceiver *Mock) Update(texture screen.Texture) {
+	mockReceiver.Called(texture)
+}
+
+func (mockScreen *Mock) NewTexture(size image.Point) (screen.Texture, error) {
+	args := mockScreen.Called(size)
+	return args.Get(0).(screen.Texture), args.Error(1)
+}
+
+func (mockTexture *Mock) Release() {
+	mockTexture.Called()
+}
+
+func (mockTexture *Mock) Upload(dp image.Point, src screen.Buffer, sr image.Rectangle) {
+	mockTexture.Called(dp, src, sr)
+}
+
+func (mockTexture *Mock) Bounds() image.Rectangle {
+	args := mockTexture.Called()
+	return args.Get(0).(image.Rectangle)
+}
+
+func (mockTexture *Mock) Fill(dr image.Rectangle, src color.Color, op draw.Op) {
+	mockTexture.Called(dr, src, op)
+}
+
+func (mockTexture *Mock) Size() image.Point {
+	args := mockTexture.Called()
+	return args.Get(0).(image.Point)
+}
+
+func (mockOperation *Mock) Do(t screen.Texture) bool {
+	args := mockOperation.Called(t)
+	return args.Bool(0)
+}
+
+func TestLoop_Post_Success(t *testing.T) {
+	textureMock := new(Mock)
+	receiverMock := new(Mock)
+	screenMock := new(Mock)
+
+	texture := image.Pt(800, 800)
+	screenMock.On("NewTexture", texture).Return(textureMock, nil)
+	receiverMock.On("Update", textureMock).Return()
+	loop := painter.Loop{
+		Receiver: receiverMock,
+	}
+
+	loop.Start(screenMock)
+
+	operationOne := new(Mock)
+	textureMock.On("Bounds").Return(image.Rectangle{})
+	operationOne.On("Do", textureMock).Return(true)
+
+	assert.Empty(t, loop.MsgQueue.Queue)
+	loop.Post(operationOne)
+	time.Sleep(1 * time.Second)
+	assert.Empty(t, loop.MsgQueue.Queue)
+
+	operationOne.AssertCalled(t, "Do", textureMock)
+	receiverMock.AssertCalled(t, "Update", textureMock)
+	screenMock.AssertCalled(t, "NewTexture", image.Pt(800, 800))
+}
+
+func TestLoop_Post_Failure(t *testing.T) {
+	textureMock := new(Mock)
+	receiverMock := new(Mock)
+	screenMock := new(Mock)
+
+	texture := image.Pt(800, 800)
+	screenMock.On("NewTexture", texture).Return(textureMock, nil)
+	receiverMock.On("Update", textureMock).Return()
+	loop := painter.Loop{
+		Receiver: receiverMock,
+	}
+
+	loop.Start(screenMock)
+
+	operationOne := new(Mock)
+	textureMock.On("Bounds").Return(image.Rectangle{})
+	operationOne.On("Do", textureMock).Return(false)
+
+	assert.Empty(t, loop.MsgQueue.Queue)
+	loop.Post(operationOne)
+	time.Sleep(1 * time.Second)
+	assert.Empty(t, loop.MsgQueue.Queue)
+
+	operationOne.AssertCalled(t, "Do", textureMock)
+	receiverMock.AssertNotCalled(t, "Update", textureMock)
+	screenMock.AssertCalled(t, "NewTexture", image.Pt(800, 800))
+}
+
+func TestLoop_Post_Multiple_Success(t *testing.T) {
+	textureMock := new(Mock)
+	receiverMock := new(Mock)
+	screenMock := new(Mock)
+
+	texture := image.Pt(800, 800)
+	screenMock.On("NewTexture", texture).Return(textureMock, nil)
+	receiverMock.On("Update", textureMock).Return()
+	loop := painter.Loop{
+		Receiver: receiverMock,
+	}
+
+	loop.Start(screenMock)
+
+	operationOne := new(Mock)
+	operationTwo := new(Mock)
+	textureMock.On("Bounds").Return(image.Rectangle{})
+	operationOne.On("Do", textureMock).Return(true)
+	operationTwo.On("Do", textureMock).Return(true)
+
+	assert.Empty(t, loop.MsgQueue.Queue)
+	loop.Post(operationOne)
+	loop.Post(operationTwo)
+	time.Sleep(1 * time.Second)
+	assert.Empty(t, loop.MsgQueue.Queue)
+
+	operationOne.AssertCalled(t, "Do", textureMock)
+	operationTwo.AssertCalled(t, "Do", textureMock)
+	receiverMock.AssertCalled(t, "Update", textureMock)
+	screenMock.AssertCalled(t, "NewTexture", image.Pt(800, 800))
+}
